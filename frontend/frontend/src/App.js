61fe5de88106ce3e153905064fca75d559ac0879
nicknilssonmoren <nicknilssonmoren@gmail.com>
2022-01-03 21:51:04 +0100

    Moved index-js to app.js

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
-import {useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import Login from './Login'
-import Header from './Header'
-import AdminPage from "./AdminPage";
+import React from 'react';
 import Register from "./Register";
-import React from "react";
-import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-// function App() {
-//     function showLogin(props) {
-//         return <Login />;
-//     }
-//
-//     function showRegister(props) {
-//         return <Register />;
-//     }
-//
-// }
-//
-// export default App;
+function App() {
+    return(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="*" element={<NoPage />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>)
+}
+
+export default App;
--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App.js"
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
-import Login from "./Login";
-import Register from "./Register";
 
-export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                    <Route path="contact" element={<Contact />} />
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
-}
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
