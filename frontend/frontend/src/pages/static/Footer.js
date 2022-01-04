03c17233905379098f2cbebd79692e9c34acb6d9
Denisé Larsson <denisee.larsson@gmail.com>
2022-01-04 11:19:48 +0100

    text align center

--- frontend/frontend/src/pages/static/Footer.js
+++ frontend/frontend/src/pages/static/Footer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
+            <div className="justify-content-center d-flex">
                 <p>Copyright@2022</p>
             </div>
         );
