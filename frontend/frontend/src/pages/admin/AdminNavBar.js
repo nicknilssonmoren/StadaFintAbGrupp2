025a460e988de0d70ac1d896d4fc6e9226221e58
Michelle Khan <michelle.khan1@outlook.com>
2022-01-06 11:37:13 +0100

    added css for admin page new design

--- frontend/frontend/src/pages/admin/AdminNavBar.js
+++ frontend/frontend/src/pages/admin/AdminNavBar.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import Header from "../static/Header";
+import {NavLink} from 'react-router-dom'
+import './AdminNavBar.css'
 
 class AdminNavBar extends Component {
     render() {
         return (
             <div>
                 <Header/>
-                <div className={"container d-flex justify-content-evenly mt-5"}>
-                    <a href={"/manageCustomer"} type="button">Hantera kunder</a>
-                    <a href={"/manageEmployee"} type="button">Hantera Städare</a>
-                    <a href={"/manageBooking"} type="button">Hantera bokningar</a>
-                    <a href={"/"} type="button">Logga ut</a>
+                <div className={"container d-flex justify-content-evenly mt-3"}>
+                    <NavLink to={"/manageCustomer"} activeClassName={"active"} exact={true} className={"adminlink"}>Hantera
+                        kunder</NavLink>
+                    <NavLink to={"/manageEmployee"} activeClassName={"active"} exact={true}
+                             className={"adminlink"}>Hantera anställda</NavLink>
+                    <NavLink to={"/manageBooking"} activeClassName={"active"} exact={true}
+                             className={"adminlink"}>Hantera bokningar</NavLink>
+                    <a href={"/"} type="button" className={"adminlink"}>Logga ut</a>
                 </div>
             </div>
         );
