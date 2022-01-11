100e685a7e8f4d3eceb5791ba1d24513e3f4e033
Michelle Khan <michelle.khan1@outlook.com>
2022-01-11 13:45:04 +0100

    radio button for done/undone booking (employee)

--- frontend/frontend/src/pages/employee/Booked.js
+++ frontend/frontend/src/pages/employee/Booked.js
@@ -16,6 +16,18 @@ class Booked extends Component {
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error mollitia natus
                                 praesentium totam! Assumenda autem debitis doloremque earum enim ipsum iusto libero
                                 molestias nemo numquam odit quaerat, veritatis, vero voluptatem.</p>
+                            <div className={"d-flex justify-content-end"}>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="inlineRadioOptions"
+                                           id="inlineRadio3" value="option3"/>
+                                    <label className="form-check-label" htmlFor="inlineRadio3">Utförd</label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="inlineRadioOptions"
+                                           id="inlineRadio4" value="option4"/>
+                                    <label className="form-check-label" htmlFor="inlineRadio4">Icke utförd</label>
+                                </div>
+                            </div>
                         </a>
                         <a href="#" className="list-group-item list-group-item-action">
                             <div className="d-flex justify-content-between">
@@ -25,6 +37,18 @@ class Booked extends Component {
                             <p className="mb-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
                                 culpa, cupiditate dicta dolor ducimus, enim est facere ipsum labore, laudantium magni
                                 neque obcaecati praesentium quam quo repudiandae saepe sunt voluptatum.</p>
+                            <div className={"d-flex justify-content-end"}>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="inlineRadioOptions"
+                                           id="inlineRadio1" value="option1"/>
+                                    <label className="form-check-label" htmlFor="inlineRadio1">Utförd</label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="inlineRadioOptions"
+                                           id="inlineRadio2" value="option2"/>
+                                    <label className="form-check-label" htmlFor="inlineRadio2">Icke utförd</label>
+                                </div>
+                            </div>
                         </a>
                     </div>
                 </div>
