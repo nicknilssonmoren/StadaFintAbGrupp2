a65e5935514ffd4a9b58d12a33b1a97a772b9559
nicknilssonmoren <nicknilssonmoren@gmail.com>
2022-01-12 14:17:11 +0100

    garbage frontend, just to try out backend

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Login from './pages/Login'
 import Register from "./pages/Register";
-import GetCustomerMall from "./pages/Backend Mall/GetCustomerMall";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Layout from "./pages/static/Layout";
 import NoPage from "./pages/NoPage";
@@ -29,7 +28,6 @@ function App() {
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<Login/>}/>
                     <Route path="register" element={<Register/>}/>
-                    <Route path="customer" element={<GetCustomerMall/>}/>
                     <Route path="admin" element={<AdminNavBar/>}/>
                     <Route path="mypage" element={<MyPage />} />
                     <Route path="managecustomer" element={<ManageCustomer/>}/>
--- frontend/frontend/src/pages/Customers/Bookings.js
+++ frontend/frontend/src/pages/Customers/Bookings.js
@@ -1,19 +1,63 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import CustomerNavBar from "./CustomerNavBar";
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 
-class Bookings extends Component {
-    render() {
-        return (
-            <div>
-                <CustomerNavBar/>
-                <div className={"d-flex justify-content-center pt-5"}>
-                    <Calendar/>
+function Bookings() {
+    const [date, setDate] = useState(new Date());
+
+    return (<>
+            <CustomerNavBar />
+            <div className='app'>
+                <br />
+                <h1 className='text-center'>Boka</h1>
+                <div className={"d-flex justify-content-center pt-5 calendar-container"}>
+                    <Calendar onChange={setDate} value={date}/>
                 </div>
+                <p className='text-center'>
+                    <span className='bold'>Selected Date:</span>{' '}
+                    {date.toDateString()}
+                </p>
+                <div className={"d-flex justify-content-center pt-5"}>
+                    <input placeholder={" Enter Email"}></input>
+                </div>
+
+                <div className={"d-flex justify-content-center pt-5"}>
+                    <div className="form-check">
+                        <input type={"radio"} name={"flexRadioDefault"} id={"flexRadioDefault1"}></input>
+                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                            Basic Städning
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input type={"radio"} name={"flexRadioDefault"} id={"flexRadioDefault1"}></input>
+                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                            Topp städning
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input type={"radio"} name={"flexRadioDefault"} id={"flexRadioDefault1"}></input>
+                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                            Diamant städning
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <input type={"radio"} name={"flexRadioDefault"} id={"flexRadioDefault1"}></input>
+                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                            Fönstertvätt
+                        </label>
+                    </div>
+                </div>
+
+                <div className={"d-flex justify-content-center pt-5"}>
+                    <button>Boka</button>
+                </div>
+
+
+
             </div>
-        );
-    }
+        </>
+    );
 }
 
 export default Bookings;
