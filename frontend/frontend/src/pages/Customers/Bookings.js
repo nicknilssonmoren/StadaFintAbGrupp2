fd3837e81f4877cbbaa67322a7ac2c1efa5308a9
nicknilssonmoren <nicknilssonmoren@gmail.com>
2022-01-12 15:06:43 +0100

    We can see the content of the input fields. Now we just need to post it to firestore.

--- frontend/frontend/src/pages/Customers/Bookings.js
+++ frontend/frontend/src/pages/Customers/Bookings.js
@@ -2,6 +2,54 @@ import React, {useState} from 'react';
 import CustomerNavBar from "./CustomerNavBar";
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
+import {createUserWithEmailAndPassword, getAuth} from "firebase/auth";
+
+function bookIt (date) {
+    let dateForMe = date;
+
+    try {
+        let email = document.getElementById("email")
+        //let myChoice = document.querySelector('input[name="cleaningChoice"]:checked').value;
+
+        let rates = document.getElementsByName('cleaningChoice');
+        let rate_value;
+        for(let i = 0; i < rates.length; i++){
+            if(rates[i].checked){
+                rate_value = rates[i].value;
+            }
+        }
+
+
+        console.log(email.value);
+        console.log(dateForMe.toDateString())
+        console.log(rate_value);
+
+
+        // const response = await fetch('http://localhost:8080/createBooking', {
+        //     mode: 'cors', method: 'post', headers: {
+        //         'Authorization': 'Bearer ' + idToken,
+        //         "Content-type": 'application/json',
+        //         "Access-Control-Allow-Origin": "http://localhost:8080/"
+        //     }, body: JSON.stringify({
+        //         "address": address, "documentId": userId, "email": email, "role": "Customer"
+        //     })
+        //
+        // })
+        //     //.then(json)
+        //     .then(function (data) {
+        //         console.log('Request succeeded with JSON response', data);
+        //     })
+        //     .catch(function (error) {
+        //         console.log('Request failed', error);
+        //     });
+        //
+        // alert("Your account has successfully been created.");
+        // window.location = '/';
+    } catch (e) {
+        alert("Email is already in use.");
+    }
+}
+
 
 function Bookings() {
     const [date, setDate] = useState(new Date());
@@ -12,48 +60,50 @@ function Bookings() {
                 <br />
                 <h1 className='text-center'>Boka</h1>
                 <div className={"d-flex justify-content-center pt-5 calendar-container"}>
-                    <Calendar onChange={setDate} value={date}/>
+                    <Calendar onChange={setDate} value={date} id={"dateForMe"}/>
                 </div>
                 <p className='text-center'>
                     <span className='bold'>Selected Date:</span>{' '}
                     {date.toDateString()}
                 </p>
                 <div className={"d-flex justify-content-center pt-5"}>
-                    <input placeholder={" Enter Email"}></input>
+                    <input placeholder={" Enter Email"} id={"email"}/>
                 </div>
 
-                <div className={"d-flex justify-content-center pt-5"}>
+                <form className={"d-flex justify-content-center pt-5"} id={"myChoice"}>
                     <div className="form-check">
-                        <input type={"radio"} name={"flexRadioDefault"} id={"flexRadioDefault1"}></input>
-                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                        <input type={"radio"} name={"cleaningChoice"} id={"basic"} value={"basic"}/>
+                        <label className="form-check-label" htmlFor="basic">
                             Basic Städning
                         </label>
                     </div>
                     <div className="form-check">
-                        <input type={"radio"} name={"flexRadioDefault"} id={"flexRadioDefault1"}></input>
-                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                        <input type={"radio"} name={"cleaningChoice"} id={"topp"} value={"topp"}/>
+                        <label className="form-check-label" htmlFor="topp">
                             Topp städning
                         </label>
                     </div>
                     <div className="form-check">
-                        <input type={"radio"} name={"flexRadioDefault"} id={"flexRadioDefault1"}></input>
-                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                        <input type={"radio"} name={"cleaningChoice"} id={"diamant"} value={"diamant"}/>
+                        <label className="form-check-label" htmlFor="diamant">
                             Diamant städning
                         </label>
                     </div>
                     <div className="form-check">
-                        <input type={"radio"} name={"flexRadioDefault"} id={"flexRadioDefault1"}></input>
-                        <label className="form-check-label" htmlFor="flexRadioDefault1">
+                        <input type={"radio"} name={"cleaningChoice"} id={"fonster"} value={"fonster"}/>
+                        <label className="form-check-label" htmlFor="fonster">
                             Fönstertvätt
                         </label>
                     </div>
-                </div>
+                </form>
 
                 <div className={"d-flex justify-content-center pt-5"}>
-                    <button>Boka</button>
+                    <button onClick={() => bookIt(date)} id={"button"} type="button"
+                            className="btn btn-primary">Boka
+                    </button>
                 </div>
 
-
+                <div id="result"></div>
 
             </div>
         </>
