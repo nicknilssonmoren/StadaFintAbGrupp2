3916cdf87175adb249107bc2d6017d7b311fc8bc
nicknilssonmoren <nicknilssonmoren@gmail.com>
2022-01-13 14:04:03 +0100

    Går nu att ta bort / avboka städningar i "Hantera bokningar" i ManageBooking.js

--- frontend/frontend/src/pages/admin/ManageBooking.js
+++ frontend/frontend/src/pages/admin/ManageBooking.js
@@ -1,25 +1,81 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AdminNavBar from "./AdminNavBar";
-import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
+import {idToken} from "../idToken";
 
+function deleteCustomerBooking(documentId) {
+    fetch('http://localhost:8080/deleteBooking?documentId=' + documentId, {
+        mode: 'cors', method: 'delete', headers: {
+            'Authorization': 'Bearer ' + idToken,
+            "Access-Control-Allow-Origin": "http://localhost:8080/"
+        }
+    })
+        //.then(json)
+        .then(function (data) {
+            console.log('Request succeeded with JSON response', data);
+        })
+        .catch(function (error) {
+            console.log('Request failed', error);
+        });
+    window.location.reload()
+}
 
 function ManageBooking() {
-    const [date, setDate] = useState(new Date());
+    const [customerBookings, setCustomerBookings] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:8080/getAllBookings', {
+            headers: {
+                'Authorization': 'Bearer ' + idToken,
+                "Access-Control-Allow-Origin": "http://localhost:8080/"
 
-    return (<>
-            <AdminNavBar />
-            <div className='app'>
-                <h1 className='text-center'>React Calendar</h1>
-                <div className={"d-flex justify-content-center pt-5 calendar-container"}>
-                    <Calendar onChange={setDate} value={date} />
-                </div>
-                <p className='text-center'>
-                    <span className='bold'>Selected Date:</span>{' '}
-                    {date.toDateString()}
-                </p>
+            }
+        })
+            .then(req => req.json())
+            .then(json => setCustomerBookings(json))
+    },[])
+
+    function getAllBookings() {
+        return customerBookings.filter(customerBookings => customerBookings.date != null)
+            .map(customerBookings => (
+                <tr key={customerBookings.documentId}>
+                    <td>{customerBookings.date}</td>
+                    <td>{customerBookings.customerEmail}</td>
+                    <td>{customerBookings.cleaningType}</td>
+                    <td>{customerBookings.address}</td>
+                    <td>
+                        <button onClick={() => deleteCustomerBooking(customerBookings.documentId)}>Avboka</button>
+                        <button>Tilldela</button>
+                        {/*
+                        TODO:
+                        Tilldela städning till anställd
+                        kanske ladda in en ny komponent där man kan välja
+                        vilken anställd som ska få städningen.
+                        */}
+                    </td>
+                </tr>))
+    }
+    return (
+        <div>
+            <AdminNavBar/>
+            <h1 className={"text-center pt-4"}>Visa Bokade Städningar</h1>
+
+            <div className={"justify-content-center d-flex pt-4"}>
+                <table className="table w-75 p-3 ">
+                    <thead>
+                    <tr>
+                        <th scope="col">Datum</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Städtyp</th>
+                        <th scope="col">Adress</th>
+                        <th scope="col">Åtgärder</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {getAllBookings()}
+                    </tbody>
+                </table>
             </div>
-        </>
+        </div>
     );
 }
 
