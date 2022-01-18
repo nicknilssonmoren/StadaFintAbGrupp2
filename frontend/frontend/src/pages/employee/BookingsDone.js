c1c93be5d5d0e304e627e6b5cf3f5d6d049c969a
nicknilssonmoren <nicknilssonmoren@gmail.com>
2022-01-18 11:53:47 +0100

    if status: "completed" the "Utförda städningar" vill display the grade the cleaning recived from the customer. If no grade has been given, the grade will be 0 until given.

--- frontend/frontend/src/pages/employee/BookingsDone.js
+++ frontend/frontend/src/pages/employee/BookingsDone.js
@@ -1,29 +1,38 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import EmployeeNavBar from "./EmployeeNavBar";
+import {idToken} from "../idToken";
 
-class BookingsDone extends Component {
-    render() {
-        return (
+function BookingsDone() {
+    const [bookings, setBookings] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:8080/getAllBookings', {
+            headers: {
+                'Authorization': 'Bearer ' + idToken
+            }
+        })
+            .then(req => req.json())
+            .then(json => setBookings(json))
+    },[])
+    function getAllFinishedBookings() {
+        return bookings.filter(booking => booking.status == "completed")
+            .map(booking => (
+                    <li className="list-group-item list-group-item-success">Bokningen genomförd och fått betyg: {booking.grade}</li>
+            ))
+    }
+
+    return (
             <div>
                 <EmployeeNavBar/>
                 <div className={"d-flex justify-content-center pt-5"}>
+                    <h5>Vid betyg: 0 är betyg ännu inte satt. Skalan går från 1-5, där 1 är lägst och 5 är högst.</h5>
+                </div>
+                <div className={"d-flex justify-content-center pt-5"}>
                     <ul className="list-group w-50">
-                        <li className="list-group-item list-group-item-success">Bokning XXX godkänd</li>
-                        <li className="list-group-item list-group-item-danger">Bokning YYY icke godkänd</li>
-                        <li className="list-group-item list-group-item-success">Bokning XXX godkänd</li>
-                        <li className="list-group-item list-group-item-success">Bokning XXX godkänd</li>
-                        <li className="list-group-item list-group-item-success">Bokning XXX godkänd</li>
-                        <li className="list-group-item list-group-item-success">Bokning XXX godkänd</li>
-                        <li className="list-group-item list-group-item-success">Bokning XXX godkänd</li>
-                        <li className="list-group-item list-group-item-success">Bokning XXX godkänd</li>
-                        <li className="list-group-item list-group-item-danger">Bokning YYY icke godkänd</li>
-                        <li className="list-group-item list-group-item-danger">Bokning YYY icke godkänd</li>
-                        <li className="list-group-item list-group-item-danger">Bokning YYY icke godkänd</li>
+                        {getAllFinishedBookings()}
                     </ul>
                 </div>
             </div>
         );
-    }
 }
 
 export default BookingsDone;
