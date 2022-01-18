2ae6389d057b66a70a978e2c964121eb9f391aef
nicknilssonmoren <nicknilssonmoren@gmail.com>
2022-01-18 14:25:04 +0100

    Layout / Nav removed

--- frontend/frontend/src/pages/static/Layout.js
+++ frontend/frontend/src/pages/static/Layout.js
@@ -4,10 +4,7 @@ const Layout = () => {
     return (
         <>
             <nav>
-                {/*<Link to="/customer">Customer</Link><br/>*/}
-                <Link to={"/admin"}>Admin</Link><br/>
-                <Link to={"/employee"}>Employee</Link><br/>
-                <Link to={"/customers"}>Customers</Link>
+
             </nav>
 
             <Outlet />
